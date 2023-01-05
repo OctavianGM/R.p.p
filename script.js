dd693af66df91d2f848f318fe662aaaf9fa9b226
OctavianGM <georgeo.manuca@gmail.com>
2023-01-05 13:38:19 +0200

    Fixed playerChoice function

--- script.js
+++ script.js
@@ -1,46 +1,57 @@
-let computerSelection;
-let computerWins=0;
-let playerWins=0;
+const choices=["rock","paper","scissors"]
+let computerChoice=0;
+let playerChoice =0;
+let playerScore=0;
+let computerScore=0;
 
 function getComputerChoice() {
-    const choices=["rock","paper","scissors"];
-computerSelection=choices[Math.floor(Math.random()*choices.length)];
-return computerSelection
+
+computerChoice=choices[Math.floor(Math.random()*choices.length)];
+return computerChoice;
 }
 
-const playerChoice=prompt("Pick rock paper or scissors");
-
-
-function playRound(playerSelection,computerSelection) {
-computerSelection=getComputerChoice();
-playerSelection=playerChoice.toLowerCase();
-    if (computerSelection===playerSelection) {
-    return "This game is a tie!";
+function getPlayerChoice() {
+playerChoice=prompt("What is your choice?").toLowerCase()
+let validation = validateInput(playerChoice)
+if (validation==true) {
+    return "your choice is " + playerChoice;
+}else {
+playerChoice=prompt(playerChoice + " is not valid. Please try again").toLowerCase()
+ return "your choice is " + playerChoice;   
 }
-    if (playerSelection==="rock") {
-        if (computerSelection==="scissors") {
-            return "player wins";
-        }   else { 
-            return "computer wins";
-        
-        }
-
-    }
-    if (playerSelection==="paper") {
-        if (computerSelection==="scissors") {
-            return "computer wins";
-        }   else { 
-            return "player wins";
-        }
-
-    }
-    if (playerSelection==="scissors") {
-        if (computerSelection==="paper") {
-            return "player wins";
-        }   else { 
-            return "computer wins";
-        }
-
-    }
 }
 
+
+
+function validateInput (playerInput) {  // check whether the input entered by the user is valid
+    if (choices.includes(playerInput)) {
+        return true;
+    } else {
+        return false;
+    }
+    
+}
+
+
+function playRound(computerChoice, playerChoice) {
+    if (computerChoice===playerChoice)
+    return "Draw"
+    if (computerChoice==="rock" && playerChoice==="paper" ||computerChoice==="paper" && playerChoice==="scissors" ||computerChoice==="scissors" && playerChoice==="rock" )
+    return "Player Wins"
+    playerScore++
+
+    if (computerChoice==="rock" && playerChoice==="scissors" || computerChoice==="paper" && playerChoice==="rock"|| computerChoice==="scissors" && playerChoice==="paper")
+    return "Computer Wins"
+    computerScore++
+}
+
+function game() {
+    for (let i=0; i <5; i++) {
+        playRound()
+        if (playerScore>computerScore) 
+    return "Player wins the game"
+    else
+    return "Computer wins the game"
+    }
+    
+}
